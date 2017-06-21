7b6731344534cd3fa8cc80c60ac4e3a117097a2e
NikolayNN <kolya.by@mail.ru>
2017-06-21 22:40:59 +0300

    #1: init commit

---
+++ src/main/java/ua/com/juja/microservices/keepers/slackbot/KeeperSlackBotApplication.java
@@ -0,0 +1,20 @@
+package ua.com.juja.microservices.keepers.slackbot;
+
+/**
+ * Created by Nikol on 6/21/2017.
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Nikolay Horushko
+ */
+
+@SpringBootApplication
+public class KeeperSlackBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeeperSlackBotApplication.class);
+    }
+}
