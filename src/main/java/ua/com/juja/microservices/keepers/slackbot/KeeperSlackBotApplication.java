fb19e3217eacac3db94baf5bacfa96b79ccf4e43
NikolayNN <kolya.by@mail.ru>
2017-07-15 15:06:16 +0300

    #7 : implemented RestUserRepository for work with User microservice. Get uuid by slackName.

--- src/main/java/ua/com/juja/microservices/keepers/slackbot/KeeperSlackBotApplication.java
+++ src/main/java/ua/com/juja/microservices/keepers/slackbot/KeeperSlackBotApplication.java
@@ -4,14 +4,42 @@ package ua.com.juja.microservices.keepers.slackbot;
  * Created by Nikol on 6/21/2017.
  */
 
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Nikolay Horushko
  */
 @SpringBootApplication
 public class KeeperSlackBotApplication {
+    @Bean
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate(httpRequestFactory());
+        restTemplate.setMessageConverters(getHttpMessageConverters());
+        return restTemplate;
+    }
+
+    private ClientHttpRequestFactory httpRequestFactory() {
+        return new HttpComponentsClientHttpRequestFactory(HttpClients.createDefault());
+    }
+
+    private List<HttpMessageConverter<?>> getHttpMessageConverters() {
+        List<HttpMessageConverter<?>> converters = new ArrayList<>();
+        converters.add(new MappingJackson2HttpMessageConverter());
+        converters.add(new StringHttpMessageConverter());
+        return converters;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(KeeperSlackBotApplication.class);
