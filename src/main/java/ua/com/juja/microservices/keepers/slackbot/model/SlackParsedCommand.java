c65e7f6cf0ef47253d5dd1e51012539043e31c44
User <netangel@inbox.ru>
2017-07-16 16:10:55 +0300

    #11 Created class structure

--- src/main/java/ua/com/juja/microservices/keepers/slackbot/model/SlackParsedCommand.java
+++ src/main/java/ua/com/juja/microservices/keepers/slackbot/model/SlackParsedCommand.java
@@ -1,19 +1,45 @@
 package ua.com.juja.microservices.keepers.slackbot.model;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
 import java.util.Map;
 
 /**
- * @author
+ * @author Konstantin Sergey
  */
 public class SlackParsedCommand {
-   private String from;
-   private String text;
-   private Map<String, UserDTO> users;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final String SLACK_NAME_PATTERN = "@([a-zA-z0-9\\.\\_\\-]){1,21}";
+    private String from;
+    private String text;
+    private Map<String, UserDTO> users;
+    private int usersCount;
 
     public SlackParsedCommand(String from, String text, Map<String, UserDTO> users) {
         this.from = from;
         this.text = text;
         this.users = users;
-        //todo task #11
+    }
+
+    public UserDTO receiveFirstUser() {
+        return null;
+    }
+
+    public List<UserDTO> receiveAllUsers() {
+        return null;
+    }
+
+    public UserDTO receiveFromUser() {
+        return users.get(from);
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public int getUsersCount() {
+        return usersCount;
     }
 }
