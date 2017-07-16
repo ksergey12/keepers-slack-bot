6f4d3ef8a45257e5182bac8216252612c13c5313
User <netangel@inbox.ru>
2017-07-16 20:14:13 +0300

    #11 Added methods

--- src/main/java/ua/com/juja/microservices/keepers/slackbot/model/SlackParsedCommand.java
+++ src/main/java/ua/com/juja/microservices/keepers/slackbot/model/SlackParsedCommand.java
@@ -2,9 +2,11 @@ package ua.com.juja.microservices.keepers.slackbot.model;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ua.com.juja.microservices.keepers.slackbot.exceptions.WrongCommandFormatException;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author Konstantin Sergey
@@ -15,20 +17,35 @@ public class SlackParsedCommand {
     private String from;
     private String text;
     private Map<String, UserDTO> users;
-    private int usersCount;
+    private List<String> slackNames;
+    private int userCount;
 
     public SlackParsedCommand(String from, String text, Map<String, UserDTO> users) {
+        if (!from.startsWith("@")) {
+            logger.debug("add '@' to slack name [{}]", from);
+            from = "@" + from;
+        }
         this.from = from;
         this.text = text;
         this.users = users;
-    }
-
-    public UserDTO receiveFirstUser() {
-        return null;
+        slackNames = receiveSlackNames();
+        userCount = slackNames.size();
     }
 
     public List<UserDTO> receiveAllUsers() {
-        return null;
+        checkSlackNamesPresence();
+        Map<String, UserDTO> usersCopy = new HashMap<>();
+        usersCopy.putAll(users);
+        usersCopy.remove(from);
+        logger.debug("Found {} users in text: [{}]", userCount, text);
+        return new LinkedList(usersCopy.values());
+    }
+
+    public UserDTO receiveFirstUser() {
+        checkSlackNamesPresence();
+        UserDTO firstUser = users.get(slackNames.get(0));
+        logger.debug("Found user: {} in text: [{}]", firstUser.toString(), text);
+        return firstUser;
     }
 
     public UserDTO receiveFromUser() {
@@ -39,7 +56,24 @@ public class SlackParsedCommand {
         return text;
     }
 
-    public int getUsersCount() {
-        return usersCount;
+    public int getUserCount() {
+        return userCount;
+    }
+
+    private List<String> receiveSlackNames() {
+        List<String> result = new ArrayList<>();
+        Pattern pattern = Pattern.compile(SLACK_NAME_PATTERN);
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            result.add(matcher.group());
+        }
+        return result;
+    }
+
+    public void checkSlackNamesPresence() {
+        if (userCount == 0) {
+            logger.warn("The text: [{}] doesn't contain slack name.");
+            throw new WrongCommandFormatException(String.format("The text '%s' doesn't contains slackName", text));
+        }
     }
 }
