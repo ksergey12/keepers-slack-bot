bebaf2f32ef29f66343b9e2382282a6fdbc84543
lyashenko_dmitriy <dmitriy.lyashenko.java@gmail.com>
2017-08-07 16:23:31 +0300

    Test

--- src/test/java/ua/com/juja/microservices/keepers/slackbot/controller/KeepersSlackCommandControllerTest.java
+++ src/test/java/ua/com/juja/microservices/keepers/slackbot/controller/KeepersSlackCommandControllerTest.java
@@ -3,7 +3,6 @@ package ua.com.juja.microservices.keepers.slackbot.controller;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
